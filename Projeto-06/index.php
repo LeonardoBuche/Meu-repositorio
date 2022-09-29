56180469bee231175ec40674977e8222dedb7c24
Leonardo Fraga <101114374+LeonardoBuche@users.noreply.github.com>
2022-09-29 10:34:33 -0300

    Add files via upload

---
+++ Projeto-06/index.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0;maximun-scale=1.0">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet">
+	<link href="css/fontawesome.css" rel="stylesheet">
+	<script src="https://kit.fontawesome.com/b5d69b6602.js" crossorigin="anonymous"></script>
+	<link href="css/style.css" rel="stylesheet" type="text/css" >
+	<title>Projeto 06</title>
+</head>
+<body>
+	<section class="main">
+		<div class="layer"></div>
+		<div class="center">
+		<header>
+			<div class="icons-social">
+				<a href="https://facebook.com"><i class="fa-brands fa-facebook"></i></a>
+				<a href="https://instagram.com"><i class="fa-brands fa-instagram"></i></a>
+
+			</div><!--icons-social-->
+			<div class="clear"></div>
+			<div class="logo">
+				Logomarca
+			</div><!--logo-->
+			<nav class="desktop">
+				<ul>
+					<li><a href="">Home</a></li>
+					<li><a href="">Sobre</a></li>
+					<li><a href="">Contato</a></li>
+				</ul>
+			</nav><!--desktop-->
+			<nav class="mobile">
+				<i class="fa fa-bars"></i>
+				<ul>
+					<li><a href="">Home</a></li>
+					<li><a href="">Sobre</a></li>
+					<li><a href="">Contato</a></li>
+				</ul>
+			</nav><!--mobile-->
+			<div class="clear"></div>
+		</header>
+		<div class="conteudo-header">
+			<h2>Bem-vindo(a) ao nosso <span>website!</span></h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent at mollis arcu. Proin tristique magna vel nunc mollis, sed porta mauris laoreet. Nunc eget risus et purus lacinia porttitor id ac purus. Morbi eleifend massa ipsum, a tincidunt magna pharetra nec. In vestibulum, odio eget accumsan ullamcorper, neque magna aliquam purus, quis sollicitudin sapien lorem in dolor. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent at mollis arcu. Proin tristique magna vel nunc mollis, sed porta mauris laoreet. Nunc eget risus et purus lacinia porttitor id ac purus.</p>
+			<a href="">Saiba mais!</a>
+
+		</div><!--conteudo-header-->
+	</div><!--center-->
+	</section><!--main-->
+
+	<section class="planos">
+	<div class="chamada-planos">
+	<div class="center">
+		<h2>Confira nossos planos</h2>
+	</div><!--center-->
+	</div><!--chamada-planos-->
+		<section class="box-planos">
+			<div class="center">
+				<div class="box-planos-single">
+					<div class="box-planos-wraper">
+					<h2>Lorem Ipsum</h2>
+					<div class="pontos-plano">
+						<p><i class="fa-solid fa-square-check"></i> Lorem ipsum dolor.</p>
+						<p><i class="fa-solid fa-square-check"></i> Lorem ipsum dolor.</p>
+						<p><i class="fa-solid fa-square-check"></i> Lorem ipsum dolor.</p>
+						<p><i class="fa-solid fa-square-check"></i> Lorem ipsum dolor.</p>
+					</div><!--pontos-plano-->
+					<div class="preco">
+						<h2>R$250,00</h2>
+					</div><!--preco-->
+				</div><!--box-planos-wraper-->
+
+				</div><!--box-planos-single-->
+
+				<div class="box-planos-single">
+					<div class="box-planos-wraper">
+					<h2>Lorem Ipsum</h2>
+					<div class="pontos-plano">
+						<p><i class="fa-solid fa-square-check"></i> Lorem ipsum dolor.</p>
+						<p><i class="fa-solid fa-square-check"></i> Lorem ipsum dolor.</p>
+						<p><i class="fa-solid fa-square-check"></i> Lorem ipsum dolor.</p>
+						<p><i class="fa-solid fa-square-check"></i> Lorem ipsum dolor.</p>
+					</div><!--pontos-plano-->
+					<div class="preco">
+						<h2>R$250,00</h2>
+					</div><!--preco-->
+					</div><!--box-planos-wraper-->
+				
+				</div><!--box-planos-single-->
+
+				<div class="box-planos-single">
+					<div class="box-planos-wraper">
+					<h2>Lorem Ipsum</h2>
+					<div class="pontos-plano">
+						<p><i class="fa-solid fa-square-check"></i> Lorem ipsum dolor.</p>
+						<p><i class="fa-solid fa-square-check"></i> Lorem ipsum dolor.</p>
+						<p><i class="fa-solid fa-square-check"></i> Lorem ipsum dolor.</p>
+						<p><i class="fa-solid fa-square-check"></i> Lorem ipsum dolor.</p>
+					</div><!--pontos-plano-->
+					<div class="preco">
+						<h2>R$250,00</h2>
+					</div><!--preco-->
+					</div><!--box-planos-wraper-->
+				
+				</div><!--box-planos-single-->
+
+				</div><!--center-->
+		</section><!--box-planos-->
+	</section><!--planos-->
+
+	<section class="parallax">
+		<div class="overlay-parallax"></div>
+		<div class="center">
+			<h2>Sobre nossa <span style="color: #2692FF;">empresa</span></h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent at mollis arcu. Proin tristique magna vel nunc mollis, sed porta mauris laoreet. Nunc eget risus et purus lacinia porttitor id ac purus. Morbi eleifend massa ipsum, a tincidunt magna pharetra nec. In vestibulum, odio eget accumsan ullamcorper, neque magna aliquam purus, quis sollicitudin sapien lorem in dolor. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque nec sem eu urna consectetur viverra sit amet nec nisi. Phasellus fermentum consequat mauris, at pretium neque vestibulum nec. Ut pulvinar ex dolor, et aliquet purus lacinia nec. Aenean a ipsum nec neque malesuada fermentum sit amet quis nisi. Morbi condimentum purus quam, ut blandit mi maximus eu. Cras tincidunt blandit metus, ut feugiat neque pulvinar sit amet. Curabitur pellentesque porttitor fringilla. Sed congue feugiat mi, vel dapibus lorem dictum at. In mollis et purus condimentum lacinia. Nullam sollicitudin ut lectus quis finibus. Nam mollis sem orci, sed consectetur ipsum imperdiet eget. Vivamus pulvinar et ex at fringilla. Donec sed eleifend purus. Nullam pharetra sapien mauris, et gravida ipsum congue eu.</p>
+		</div><!--center-->
+	</section><!--parallax-->
+	<section class="final-section">
+		<div class="center">
+			<div class="w50 contato">
+				<h2><div class="borda-efeito"></div> Entre em contato</h2>
+				<form>
+					<input type="text" placeholder="Insira seu nome">
+					<input type="email" placeholder="Insira seu Email">
+					<textarea placeholder="Sua mensagem"></textarea>
+					<input type="submit" value="Enviar">
+				</form>
+			</div>
+
+			<div class="w50 time">
+				<h2><div class="borda-efeito"></div> Conheça nosso time</h2>
+					<div class="w50 equipe-single">
+					<div class="topo-box-usuario">
+					<div class="box-avatar"></div><div class="info-usuario">
+						<p style="font-weight: bold;">Leonardo Fraga</p>
+						<p style="font-weight: italic;">Desenvolvedor</p>
+					</div>
+			</div><!--topo-box-usuario-->
+			<div class="descricao-usuario-box">
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque nec sem eu urna consectetur viverra sit amet nec nisi. Phasellus fermentum consequat mauris, at pretium neque vestibulum nec. Ut pulvinar ex dolor, et aliquet purus lacinia nec.</p>
+			</div><!--descricao-usuario-box-->
+			</div>
+
+					<div class="w50 equipe-single">
+					<div class="topo-box-usuario">
+					<div class="box-avatar"></div><div class="info-usuario">
+						<p style="font-weight: bold;">Leonardo Fraga</p>
+						<p style="font-weight: italic;">Desenvolvedor</p>
+					</div>
+			</div><!--topo-box-usuario-->
+			<div class="descricao-usuario-box">
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque nec sem eu urna consectetur viverra sit amet nec nisi. Phasellus fermentum consequat mauris, at pretium neque vestibulum nec. Ut pulvinar ex dolor, et aliquet purus lacinia nec.</p>
+			</div><!--descricao-usuario-box-->
+			</div>
+
+			<div class="clear"></div>
+			</div>
+			<div class="clear"></div>
+			
+		</div>
+	</section><!--final-section-->
+	<footer>
+		<p>Todos os direitos reservados.</p>
+		
+	</footer>
+
+
+</body>
+</html>
