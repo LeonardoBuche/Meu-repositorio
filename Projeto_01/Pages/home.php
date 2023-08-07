ec10827022524d57c7e46ec2def1d949e75a81ff
Leonardo Fraga <101114374+LeonardoBuche@users.noreply.github.com>
2023-08-07 09:39:23 -0300

    Add files via upload

---
+++ Projeto_01/Pages/home.php
@@ -0,0 +1,89 @@
+<section class="banner-container">
+	<div style="background-image: url('<?php echo INCLUDE_PATH; ?>Imagens/bg-form.jpg');" class="banner-single"></div><!--banner-single-->
+	<div style="background-image: url('<?php echo INCLUDE_PATH; ?>Imagens/bg-form2.jpg');" class="banner-single"></div><!--banner-single-->
+	<div style="background-image: url('<?php echo INCLUDE_PATH; ?>Imagens/bg-form3.jpg');" class="banner-single"></div><!--banner-single-->
+		<div class="overlay"></div>
+		<div class="center">
+		<form>
+			<h2>Insira seu E-mail</h2>
+			<input type="email" name="email" required>
+			<input type="submit" name="açao" value="Cadastrar">
+		</form>
+	</div><!--center-->
+	<div class="bullets">
+		
+
+	</div><!--bullets-->
+	</section><!--banner-principal-->
+
+	<section class="descricao-autor">
+		<div class="center">
+		<div class="w50 left">
+			<h2>Leonardo B. Fraga</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse et velit ut ex ornare pretium. Aenean est risus, molestie sagittis nisi ut, rutrum eleifend lectus. Aliquam erat volutpat. Suspendisse non tristique risus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Duis suscipit euismod arcu, a consectetur ligula sagittis vitae. Phasellus rutrum vestibulum ligula, ut ultrices neque volutpat in. Ut lobortis rhoncus lacus at venenatis. Duis diam quam, tempor non aliquet eget, auctor non est. Phasellus ornare nec risus nec feugiat. Pellentesque rutrum, mi a pharetra ultrices, purus arcu bibendum nibh, fermentum faucibus neque enim quis arcu. Donec dolor neque, condimentum a felis in, scelerisque ultricies erat. Fusce lacinia ipsum facilisis lorem viverra, quis scelerisque magna feugiat. Nulla facilisi. Suspendisse potenti.</p>
+
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse et velit ut ex ornare pretium. Aenean est risus, molestie sagittis nisi ut, rutrum eleifend lectus. Aliquam erat volutpat. Suspendisse non tristique risus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Duis suscipit euismod arcu, a consectetur ligula sagittis vitae. Phasellus rutrum vestibulum ligula, ut ultrices neque volutpat in. Ut lobortis rhoncus lacus at venenatis. Duis diam quam, tempor non aliquet eget, auctor non est. Phasellus ornare nec risus nec feugiat. Pellentesque rutrum, mi a pharetra ultrices, purus arcu bibendum nibh, fermentum faucibus neque enim quis arcu. Donec dolor neque, condimentum a felis in, scelerisque ultricies erat. Fusce lacinia ipsum facilisis lorem viverra, quis scelerisque magna feugiat. Nulla facilisi. Suspendisse potenti.</p>
+
+		</div><!--w50-->
+
+		<div class="w50 left">
+			<img class="right" src="<?php echo INCLUDE_PATH; ?>Imagens/perfil.jpg">
+
+		</div><!--w50-->
+		<div class="clear"></div>
+	</div><!--center-->
+
+	</section><!--descricao-autor-->
+
+	<section class="especialidades">
+		<div class="center">
+			<h2 class="title">Especialidades</h2>
+			<div class="w33 left box-especialidade">
+				<h3><i class="fa-brands fa-css3"></i></h3>
+				<h4>CSS3</h4>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse et velit ut ex ornare pretium. Aenean est risus, molestie sagittis nisi ut, rutrum eleifend lectus. Aliquam erat volutpat. Suspendisse non tristique risus.</p>
+			</div><!--box-especialidade-->
+			<div class="w33 left box-especialidade">
+				<h3><i class="fa-brands fa-html5"></i></h3>
+				<h4>HTML5</h4>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse et velit ut ex ornare pretium. Aenean est risus, molestie sagittis nisi ut, rutrum eleifend lectus. Aliquam erat volutpat. Suspendisse non tristique risus.</p>
+			</div><!--box-especialidade-->
+			<div class="w33 left box-especialidade">
+				<h3><i class="fa-brands fa-square-js"></i></h3>
+				<h4>JavaScript</h4>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse et velit ut ex ornare pretium. Aenean est risus, molestie sagittis nisi ut, rutrum eleifend lectus. Aliquam erat volutpat. Suspendisse non tristique risus.</p>
+			</div><!--box-especialidade-->
+			<div class="clear"></div>
+		</div><!--center-->
+	</section><!--especialidades-->
+
+	<section class="extras">
+		<div class="center">
+			<div id="depoimentos" class="w50 left depoimentos-container">
+				<h2 class="title">Depoimentos</h2>
+				<div class="depoimento-single">
+					<p class="depoimento-descricao">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse et velit ut ex ornare pretium. Aenean est risus, molestie sagittis nisi ut, rutrum eleifend lectus. Aliquam erat volutpat. Suspendisse non tristique risus."</p>
+					<p class="nome-autor">Lorem Ipsum</p>
+				</div><!--depoimento-single-->
+				<div class="depoimento-single">
+					<p class="depoimento-descricao">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse et velit ut ex ornare pretium. Aenean est risus, molestie sagittis nisi ut, rutrum eleifend lectus. Aliquam erat volutpat. Suspendisse non tristique risus."</p>
+					<p class="nome-autor">Lorem Ipsum</p>
+				</div><!--depoimento-single-->
+				<div class="depoimento-single">
+					<p class="depoimento-descricao">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse et velit ut ex ornare pretium. Aenean est risus, molestie sagittis nisi ut, rutrum eleifend lectus. Aliquam erat volutpat. Suspendisse non tristique risus."</p>
+					<p class="nome-autor">Lorem Ipsum</p>
+				</div><!--depoimento-single-->
+			</div><!--w50-->
+			<div id="servicos" class="w50 left servicos-container">
+				<h2 class="title">Serviços</h2>
+				<div class="servicos">
+				<ul>
+					<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse et velit ut ex ornare pretium.</li>
+					<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse et velit ut ex ornare pretium.</li>
+					<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse et velit ut ex ornare pretium.</li>
+				</ul>
+			</div><!--servicos-->
+			</div><!--w50-->
+			<div class="clear"></div>
+		</div><!--center-->
+	</section><!--extras-->
